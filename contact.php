6710701ebc6f20eb9f0957403b25e70a563a0f04
blaycoder <onatola4real@gmail.com>
2022-02-24 14:08:22 +0100

    commit on 'master'

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@ echo $name = trim($_POST['name']);
 echo $subject_msg = trim($_POST['subject_msg']);
 echo $message = trim($_POST['message']);
 
-$emailTo = 'themesawesome@gmail.com';
+$emailTo = 'divineornament@yahoo.com';
 $subject = 'Request for contact has been made';
 $body = "Name: $name \r\nEmail: $email \r\nSubject: $subject_msg \r\nMessage: $message";
 $headers = 'From: ' .' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
